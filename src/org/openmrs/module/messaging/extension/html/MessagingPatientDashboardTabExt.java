42976c7fea9593b7e0d4187070eb64e407500ac7
Dieterich Lawson <dieterich.lawson@gmail.com>
2010-03-30 20:52:47 +0000

    messaging module: Adding a Patient Dashboard Tab extension for the messaging module. Also removing an admin list extension point that I thought I removed in the last commit...

---
+++ src/org/openmrs/module/messaging/extension/html/MessagingPatientDashboardTabExt.java
@@ -0,0 +1,33 @@
+package org.openmrs.module.messaging.extension.html;
+
+import org.openmrs.module.Extension;
+import org.openmrs.module.messaging.MessagingConstants;
+import org.openmrs.module.web.extension.PatientDashboardTabExt;
+
+public class MessagingPatientDashboardTabExt extends PatientDashboardTabExt {
+
+	public Extension.MEDIA_TYPE getMediaType(){
+		return Extension.MEDIA_TYPE.html;
+	}
+	
+	@Override
+	public String getPortletUrl() {
+		return "messagesPatientDashboardTab";
+	}
+
+	@Override
+	public String getRequiredPrivilege() {
+		return MessagingConstants.PRIV_VIEW_MESSAGES;
+	}
+
+	@Override
+	public String getTabId() {
+		return "messages";
+	}
+
+	@Override
+	public String getTabName() {
+		return "Messages";
+	}
+
+}
