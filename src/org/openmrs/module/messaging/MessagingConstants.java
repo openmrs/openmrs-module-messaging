237513cc06dd8351dae4d4abeaefd614c782482c
Dieterich Lawson <dieterich.lawson@gmail.com>
2010-03-30 20:49:34 +0000

    messaging module: changed the name of 2 privileges so as to not conflict with existing ones

--- src/org/openmrs/module/messaging/MessagingConstants.java
+++ src/org/openmrs/module/messaging/MessagingConstants.java
@@ -5,6 +5,6 @@ public final class MessagingConstants {
 	public static final String PRIV_VIEW_MESSAGES = "View Messages";
 	public static final String PRIV_SEND_MESSAGES = "Send Messages";
 	public static final String PRIV_MANAGE_MESSAGES= "Manage Messages";
-	public static final String PRIV_VIEW_ADRESSES = "View Addresses";
-	public static final String PRIV_MANAGE_ADRESSES = "Manage Addresses";
+	public static final String PRIV_VIEW_ADRESSES = "View Messaging Addresses";
+	public static final String PRIV_MANAGE_ADRESSES = "Manage Messaging Addresses";
 }
