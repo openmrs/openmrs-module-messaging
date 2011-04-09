da5bea2538ffc197cd3cd4c7507607d17eeadc6f
Dieterich Lawson <dieterich.lawson@gmail.com>
2011-04-09 21:40:45 +0000

    Removing unused "BadCredentialsException"

--- api/src/main/java/org/openmrs/module/messaging/domain/gateway/exception/BadCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package org.openmrs.module.messaging.domain.gateway.exception;
-
-@SuppressWarnings("serial")
-public class BadCredentialsException extends Exception{
-	public BadCredentialsException(String message){
-		super(message);
-	}
-}
